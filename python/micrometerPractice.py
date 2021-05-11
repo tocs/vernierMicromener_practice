cec337c6bbfc901a42117cf480f89d6f9d632951
tocs <gscotfleming@yahoo.com>
2021-05-11 12:10:41 -0500

    Back to normal.

--- python/micrometerPractice.py
+++ python/micrometerPractice.py
@@ -1,60 +1,66 @@
 import Image, ImageDraw
-import numpy as np
 import sys
 
-args = sys.argv
-if len(args) > 1:
-    length = float(args[-1])
-else:
-    length = 0.3799
-
-def sleeveStuff(): #, sleeveYZero):
-    sleevePoint = [sleevePosition(length), sleeveYZero]
-    pic = ImageDraw.Draw(sleeveImg)
-    pic.ellipse([sleevePoint[0] - 10, sleevePoint[1] - 10, sleevePoint[0] + 10, sleevePoint[1] + 10], fill = (255,0,0))
-    del pic
-    return(sleevePoint)
-
-# Sleeve stuff
-sleeveFile = "./vernierScales_sleeveImg.png"
-sleeveImg = Image.open(sleeveFile)
-sleeveEnds = [250, 4250]
-sleeveRange = [0.0, 1.0]
-sleeveYZero = sleeveImg.size[1] - 820.0
-sleevePosition = (lambda length: sleeveEnds[0] + length * ( sleeveEnds[1] - sleeveEnds[0]))
-sleevePoint = sleeveStuff()
 
 
-# Thimble stuff
-def thimbleStuff():
-    pic = ImageDraw.Draw(thimbleImg)
-    pic.ellipse([thimblePoint[0] - 10, thimblePoint[1] - 10, thimblePoint[0] + 10,
-                 thimblePoint[1] + 10], fill = (255,0,0))
-    del pic
-    sleeveImg.paste(thimbleImg,
-                    (int(sleevePoint[0]),
-                     int(sleeveYZero - thimblePoint[1])))
-    print "top of thimble: ", int(sleeveYZero - thimblePoint[1])
+def micrometerReading(length,
+                      sleeveFile = "./vernierScales_sleeveImg.png",
+                      sleeveEnds = [250, 4250],
+                      sleeveRange = [0.0, 1.0],
+                      thimbleFile = "./vernierScales_thimbleImg.png",
+                      thimbleRange = [0, 0.0250],
+                      thimbleYZero = 2068,
+                      thimbleEnds = [2068.0, 68.0],
+                      ):
 
-    # if thimble image does not fill top of image past copy above
-    if (int(sleeveYZero - thimblePoint[1]) > 0) and (int(sleeveYZero - thimblePoint[1]) < sleeveImg.size[1]):
+    def sleeveStuff(): #, sleeveYZero):
+        sleevePoint = [sleevePosition(length), sleeveYZero]
+        pic = ImageDraw.Draw(sleeveImg)
+        print sleevePoint
+        pic.ellipse([sleevePoint[0] - 10, sleevePoint[1] - 10, sleevePoint[0] + 10, sleevePoint[1] + 10], fill = (255,0,0))
+        del pic
+        return(sleevePoint)
+
+    def thimbleStuff():
+        pic = ImageDraw.Draw(thimbleImg)
+        pic.ellipse([thimblePoint[0] - 10, thimblePoint[1] - 10, thimblePoint[0] + 10,
+                     thimblePoint[1] + 10], fill = (255,0,0))
+        del pic
         sleeveImg.paste(thimbleImg,
                         (int(sleevePoint[0]),
-                         136 + int(sleeveYZero - thimblePoint[1]) - thimbleImg.size[1]))
-    # if thimble image does not fill bottom of image past copy below
-    if thimbleImg.size[1] + int(sleeveYZero - thimblePoint[1]) < sleeveImg.size[1]:
-        print "pasting below"
-        sleeveImg.paste(thimbleImg,
-                        (int(sleevePoint[0]),
-                         int(sleeveYZero - thimblePoint[1]) + thimbleImg.size[1] - 136))
+                         int(sleeveYZero - thimblePoint[1])))
 
-thimbleFile = "./vernierScales_thimbleImg.png"
-thimbleImg = Image.open(thimbleFile)
-thimbleRange = [0, 0.0250]
-thimbleYZero = 2068
-thimbleEnds = [2068.0, 68.0]
-thimblePosition = (lambda pos: thimbleEnds[0] - ((pos % 0.025) * (thimbleEnds[0] - thimbleEnds[1])) / 0.025)        
-thimblePoint = [0, thimblePosition(length)]
-thimbleStuff()
+        # if thimble image does not fill top of image past copy above
+        if (int(sleeveYZero - thimblePoint[1]) > 0) and (int(sleeveYZero - thimblePoint[1]) < sleeveImg.size[1]):
+            sleeveImg.paste(thimbleImg,
+                            (int(sleevePoint[0]),
+                             136 + int(sleeveYZero - thimblePoint[1]) - thimbleImg.size[1]))
+        # if thimble image does not fill bottom of image past copy below
+        if thimbleImg.size[1] + int(sleeveYZero - thimblePoint[1]) < sleeveImg.size[1]:
+            print "pasting below"
+            sleeveImg.paste(thimbleImg,
+                            (int(sleevePoint[0]),
+                             int(sleeveYZero - thimblePoint[1]) + thimbleImg.size[1] - 136))
 
-sleeveImg.save("test%f.png" % length)
+
+    
+    sleevePosition = (lambda length: sleeveEnds[0] + length * ( sleeveEnds[1] - sleeveEnds[0]))
+    thimblePosition = (lambda pos: thimbleEnds[0] - ((pos % 0.025) * (thimbleEnds[0] - thimbleEnds[1])) / 0.025)
+    thimbleImg = Image.open(thimbleFile)
+    sleeveImg = Image.open(sleeveFile)
+    thimblePoint = [0, thimblePosition(length)]
+    sleeveYZero = sleeveImg.size[1] - 820.0
+    sleevePoint = sleeveStuff()
+    thimbleStuff()
+    sleeveImg.save("test%f.png" % length)
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    if len(args) > 1:
+        length = float(args[-1])
+    else:
+        length = 0.5000
+
+    micrometerReading(length)
+# just changing file.
--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
-Simple project to practice reading a micrometer with a verier scale.
-- ./svg
-- ./python
-- ./readme.txt
 
-Usage:
-prompt$ python micrometerPractice.py [length]
-	length: A decimal between 0.0 and 1.0. (default 0.5000).
+Simple project to practice reading a micrometer with a verier scale.
+    ./svg
+    ./python
+    ./readme.txt
+
+Usage: <br>
+prompt$ python micrometerPractice.py [length] length: A decimal between 0.0 and 1.0. (default 0.5000). 
+
 Makes a png image of the representation of a inch micrometer scale of the length given. Includes a vernier scale.
